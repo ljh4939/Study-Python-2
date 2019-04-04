8eb903c05ad6a5c0161b812cdd8cd506618dcd41
lgh4939 <oox3651@naver.com>
2019-04-04 19:41:35 +0900

    추가

---
+++ src/ch1/8.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.shape("trutle")
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
---
+++ src/ch1/9.py
@@ -0,0 +1,5 @@
+import turtle
+turtle.shape("turtle")
+for i in range(4):
+    turtle.right(90)
+    turtle.forward(100)
